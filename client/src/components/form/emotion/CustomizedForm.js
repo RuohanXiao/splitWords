9711f5525eb708f698d832d278a733a568346dc4
xrh <402922811@qq.com>
2018-08-15 11:52:33 +0800

    ''

--- client/src/components/form/emotion/CustomizedForm.js
+++ client/src/components/form/emotion/CustomizedForm.js
@@ -96,110 +96,38 @@ class CustomizedForm extends Component{
             </Row>
             <Row>
               <Col>
-              <Content>
-                <Content style={{textAlign: 'center',margin: '2%'}}>
-                  {/* <Input onChange={ titleChange } value={ createArticleTitle } placeholder="输入标题..." style={{ width: '60%', marginBottom: '2%' }}/>
-                  <TextArea onChange={ contentChange } value={ createArticle } autosize = {{ minRows: 15 }} style={{ width: '60%' }} placeholder = "输入文章内容..."/> */}
-                  {upDateFlag.isUpdate?
-                    <Input disabled={true} placeholder="请选择" style={{ width: '90%', marginBottom: '2%' }}/>
-                    :
-                    <Input placeholder="请输入标题" style={{ width: '90%', marginBottom: '2%' }}/>
-                  }
-                  {upDateFlag.isUpdate?
-                    <Input disabled={true} placeholder="标注分类" style={{ width: '90%', marginBottom: '2%' }}/>
-                    :
-                    <Select
-                        mode="multiple"
-                        style={{ width: '90%', marginBottom: '2%' }}
-                        placeholder="Please select"
-                        defaultValue={[]}
-                        onChange={this.handleChange}
-                    >
-                        {children}
-                    </Select>
-                  }
-                  {upDateFlag.isUpdate?
-                    <TextArea  autosize = {{ minRows: 15 }} style={{ width: '90%',readonly:"readonly" }} value="1231321324561321315431" placeholder = "未找到文章内容"/>
-                    :
-                    <TextArea  autosize = {{ minRows: 15 }} style={{ width: '90%' }} placeholder = "请输入文章内容..."/>
-                  }
-                  
+                <Content>
+                  <Content style={{textAlign: 'center',margin: '2%'}}>
+                    {/* <Input onChange={ titleChange } value={ createArticleTitle } placeholder="输入标题..." style={{ width: '60%', marginBottom: '2%' }}/>
+                    <TextArea onChange={ contentChange } value={ createArticle } autosize = {{ minRows: 15 }} style={{ width: '60%' }} placeholder = "输入文章内容..."/> */}
+                    {upDateFlag.isUpdate?
+                      <Input disabled={true} placeholder="请选择" style={{ width: '90%', marginBottom: '2%' }}/>
+                      :
+                      <Input placeholder="请输入标题" style={{ width: '90%', marginBottom: '2%' }}/>
+                    }
+                    {upDateFlag.isUpdate?
+                      <Input disabled={true} placeholder="标注分类" style={{ width: '90%', marginBottom: '2%' }}/>
+                      :
+                      <Select
+                          mode="multiple"
+                          style={{ width: '90%', marginBottom: '2%' }}
+                          placeholder="Please select"
+                          defaultValue={[]}
+                          onChange={this.handleChange}
+                      >
+                          {children}
+                      </Select>
+                    }
+                    {upDateFlag.isUpdate?
+                      <TextArea  autosize = {{ minRows: 15 }} style={{ width: '90%',readonly:"readonly" }} value="1231321324561321315431" placeholder = "未找到文章内容"/>
+                      :
+                      <TextArea  autosize = {{ minRows: 15 }} style={{ width: '90%' }} placeholder = "请输入文章内容..."/>
+                    }
                   </Content>
-                {/* <Footer style={{textAlign: 'center', marginTop: '3%'}}>
-                  <Button type="primary" onClick={create}>确认</Button>
-                  <Button type="primary" style={{ marginLeft: '20px'}} onClick={cancel}>取消</Button>
-                </Footer> */}
-              </Content>
+                </Content>
               </Col>
             </Row>
-                {/* <Form layout="horizontal">
-                    <FormItem label="姓名" {...FormItemLayout} hasFeedback>
-                        {getFieldDecorator('name', {
-                            rules: [{ required: true, message: '请输入姓名！' }],
-                        })(
-                            <Input />
-                        )}
-                    </FormItem>
-                    <FormItem label="性别" {...FormItemLayout} hasFeedback>
-                        {getFieldDecorator('sex', {
-                            rules: [{ required: true, message: '请选择性别！' }],
-                        })(
-                            <Radio.Group>
-                                <Radio value='男'>男</Radio>
-                                <Radio value='女'>女</Radio>
-                            </Radio.Group>
-                        )}
-                    </FormItem>
-                    <FormItem label="年龄" {...FormItemLayout} hasFeedback>
-                        {getFieldDecorator('age', {
-                            rules: [{ required: true, message: '请输入年龄！' }],
-                        })(
-                            <InputNumber min={0} max={199} step={1} />
-                        )}
-                    </FormItem>
-                    <FormItem label="地址" {...FormItemLayout} hasFeedback>
-                        {getFieldDecorator('address', {
-                            rules: [{ required: true, message: '请选择地址！' }],
-                        })(
-                            <Cascader options={options}/>
-                        )}
-                    </FormItem>
-                    <FormItem label="手机号" {...FormItemLayout} hasFeedback>
-                        {getFieldDecorator('phone', {
-                            rules: [{
-                                pattern: /^1(3|4|5|7|8)\d{9}$/, message: "手机号码格式不正确！"
-                            },{
-                                required: true, message: '请输入手机号！'
-                            }],
-                        })(
-                            <Input addonBefore={PhoneBefore} style={{ width: '100%' }} />
-                        )}
-                    </FormItem>
-                    <FormItem label="邮箱" {...FormItemLayout} hasFeedback>
-                        {getFieldDecorator('email', {
-                            rules: [{
-                                type: 'email', message: '邮箱格式不正确！',
-                            }, {
-                                required: true, message: '请输入邮箱！',
-                            }],
-                        })(
-                            <Input />
-                        )}
-                    </FormItem>
-                    <FormItem label="网址" {...FormItemLayout} hasFeedback>
-                        {getFieldDecorator('website', {
-                            rules: [{required: true, message: '请输入网址！'}],
-                        })(
-                            <AutoComplete
-                                dataSource={websiteOptions}
-                                onChange={this.handleWebsiteChange}
-                            >
-                                <Input/>
-                            </AutoComplete>
-                        )}
-                    </FormItem>
-                </Form> */}
-            </Modal>
+          </Modal>
         );
     }
 }
--- client/state/reducer.js
+++ client/state/reducer.js
@@ -1,104 +1,15 @@
-const initialState = {
-  visible: false,
-  radioValue: -1,
-  wordsType: ['无', '名词', '动词', '形容词'],
-  color: ['black', 'red', 'green', 'blue'],
-  typeArr: [0, 1, 2, 3],
-  page: 1,
-  totalCount: 0,
-  path: 'http://localhost:3000',
-  selectedKeys: [],
-  articles: [
-    { 
-      id: null,
-      title: '',
-      content: '',
-      separateWords: [],
-      separateWordsProperty: [],
-      markEntity: []
-    }
-  ],
-  selection: {
-    content: '',
-    start: 0,
-    end: 0
-  },
-  showArticle: { 
-    id: null,
-    title: '',
-    content: '',
-    separateWords: [],
-    separateWordsProperty: [],
-    markEntity: []
-  },
-  createArticle: '',
-  createArticleTitle: ''
+const initState = {
+  // 里面是初始化数据
 };
-
-const reducer = (state = initialState, action) => {
+//利用Object.assign 重组object并返回
+const reducer = (state = initState, action) => {
   switch (action.type) {
-    case "OPEN_MODAL": 
-      return Object.assign({}, state, {
-        ...state, 
-        visible: true    // 需要修改的值放在后面，覆写前面 state 中相同的值
-      })
-    case "CLOSE_MODAL": 
-      return Object.assign({}, state, {
-        ...state,
-        visible: false
-      })
-    case "SET_SELECTION":
-      return Object.assign({}, state, {
-        ...state,
-        selection: action.selection
-      })   
-    case "SET_RADIO_VALUE":
-      return Object.assign({}, state, {
-        ...state,
-        radioValue: action.radioValue
-      }) 
-    case "SET_SHOWARTICLE": {
-      return Object.assign({}, state, {
-        ...state,
-        showArticle: action.showArticle
-      })
-    }
-    case "SET_ARTICLES": {
-      return Object.assign({}, state, {
-        ...state,
-        articles: action.articles
-      })      
-    }
-    case "SET_SELECTED_KEYS": {
-      return Object.assign({}, state, {
-        ...state,
-        selectedKeys: action.selectedKeys
-      })      
-    }
-    case "SET_TOTAL_COUNT": {
-      return Object.assign({}, state, {
-        ...state,
-        totalCount: action.totalCount
-      })      
-    }
-    case "SET_PAGE": {
-      return Object.assign({}, state, {
-        ...state,
-        page: action.page
-      })  
-    }
-    case "SET_CREATE_ARTICLE": {
-      return Object.assign({}, state, {
-        ...state,
-        createArticle: action.createArticle
-      })  
-    }
-    case "SET_CREATE_ARTICLE_TITLE": {
-      return Object.assign({}, state, {
-        ...state,
-        createArticleTitle: action.createArticleTitle
-      })  
-    }
+    // 例子：
+    // case "OPEN_MODAL": 
+    //   return Object.assign({}, state, {
+    //     ...state, 
+    //     visible: true    // 需要修改的值放在后面，覆写前面 state 中相同的值
+    //   })
     default:
       return state;
   }
